2e5857c3e1d9d29547705d7a3ae8a60bf1bfe033
Yaniss CHNAKIB <ychnakib@e3r10p1.42.fr>
2013-11-19 16:42:07 +0100

    Avec des vrais morceaux de .h dedans

---
+++ lib/libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ychnakib <ychnakib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/19 12:38:35 by ychnakib          #+#    #+#             */
+/*   Updated: 2013/11/19 15:02:49 by ychnakib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <string.h>
+#include <stdio.h>
+
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *restrict s1, const void *restrict s2, size_t n);
+void	*ft_memccpy(void *restrict s1, const void *restrict s2, int c, size_t n);
+#endif
